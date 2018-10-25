f64084e587b272b9095739c873188c47c80a22a7
bshpetner00 <bshpetner00@stuy.edu>
2018-10-25 10:04:39 -0400

    debut of some rudimentary functions

--- SuperArray.java
+++ SuperArray.java
@@ -1,7 +1,38 @@
 public class SuperArray {
   private String[] data;
   private int size;
-  public static void clear() {
-
+  public SuperArray() {
+    size = 0;
+    data = new String[10];
   }
+  public void clear() {
+    size = 0;
+  }
+  public int size() {
+    return data.length;
+  }
+  public boolean isEmpty() {
+    return data.length > 0;
+  }
+  public String toStringDebug() {
+    String joke = "[";
+    for (String x:data) {
+      joke += x;
+      joke += ", ";
+    }
+    joke += "]";
+    return joke;
+  }
+  public String toString() {
+    String joke = "[";
+    for (String x:data) {
+      if (x.equals(null) == false) {
+        joke += x;
+        joke += ", ";
+      }
+    }
+    joke += "]";
+    return joke;
+  }
+
 }
